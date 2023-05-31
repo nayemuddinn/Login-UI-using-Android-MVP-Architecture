1276e8339c55e587871dc1c7214e5c49841a30bd
nayemuddinn <nayem.uddin332@gmail.com>
2023-05-31 20:11:42 +0600

    Added ProgressBar

--- app/src/main/java/com/example/loginuiusingmvppattern/MainActivity.java
+++ app/src/main/java/com/example/loginuiusingmvppattern/MainActivity.java
@@ -5,15 +5,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 
 public class MainActivity extends AppCompatActivity {
      EditText userName,passWord;
      Button button;
+     ProgressBar progressBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+         progressBar=findViewById(R.id.Progress_bar);
          userName=findViewById(R.id.username);
          passWord=findViewById(R.id.password);
          button=findViewById(R.id.button);
