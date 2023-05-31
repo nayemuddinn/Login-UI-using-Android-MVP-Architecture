69bd6f46b82be69104c19299ac693d5ca8eb7a64
nayemuddinn <nayem.uddin332@gmail.com>
2023-05-31 19:59:47 +0600

    Second Commit

--- app/src/main/java/com/example/loginuiusingmvppattern/MainActivity.java
+++ app/src/main/java/com/example/loginuiusingmvppattern/MainActivity.java
@@ -3,12 +3,23 @@ package com.example.loginuiusingmvppattern;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+     EditText userName,passWord;
+     Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+         userName=findViewById(R.id.username);
+         passWord=findViewById(R.id.password);
+         button=findViewById(R.id.button);
+
+
+
+
     }
 }
